da7be94a37ca11a0268288ff1f3e2b5352756e83
Hubert Droździak <hubertdrozdziak@gmail.com>
2022-04-09 01:56:55 +0200

    Task5 complete

--- main.cpp
+++ main.cpp
@@ -35,16 +35,16 @@ bool areaLessThan10(shared_ptr<Shape> s)
 
 void printCollectionElements(const Collection& collection)
 {
-    for(auto it = collection.begin(); it != collection.end(); ++it)
-        if(*it)
-            (*it)->print();
+    for(const auto & it : collection)
+        if(it)
+            it->print();
 }
 
 void printAreas(const Collection& collection)
 {
-    for(auto it = collection.begin(); it != collection.end(); ++it)
-        if(*it)
-            cout << (*it)->getArea() << std::endl;
+    for(const auto & it : collection)
+        if(it)
+            cout << it->getArea() << std::endl;
 }
 
 void findFirstShapeMatchingPredicate(const Collection& collection,
