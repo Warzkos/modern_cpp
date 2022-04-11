18295d65992125b5243cf09b4060e20c6f3592d2
Hubert Droździak <hubertdrozdziak@gmail.com>
2022-04-11 23:53:40 +0200

    Task7 fixed and complete

--- Square.cpp
+++ Square.cpp
@@ -11,17 +11,17 @@ Square::Square(const Square &other)
 {}
 */
 
-double Square::getArea()
+double Square::getArea() const
 {
     return getX() * getX();
 }
 
-double Square::getPerimeter()
+double Square::getPerimeter() const
 {
     return 4 * getX();
 }
 
-void Square::print()
+void Square::print() const
 {
     std::cout << "Square:      x: " << getX() << std::endl
               << "          area: " << getArea() << std::endl
